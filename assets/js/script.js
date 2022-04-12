33c9eaebf7ea117ec39f96b5f7aedf5c55632446
Ady <ady@MacBook-Pro-de-Ady.local>
2022-04-12 15:28:19 +0200

    Ajout bouton retour top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-//  window.onscroll = function() {scrollFunction()};
 
-//  function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//      document.getElementById("navScroll").style.height = "70px";
-    
-//    } else {
-//      document.getElementById("navScroll").style.height = "auto";
-//    }
-//  }
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 $("nav ul a").on("click", function (e) {
   // 1
   e.preventDefault();
